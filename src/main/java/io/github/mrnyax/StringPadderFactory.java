95123d2fccd014072387d313a6a856f585ec9a2f
Patrick Nyakoria <patrick.nyakoria@acrolinx.com>
2022-05-27 10:06:22 +0300

    feat: add classes

---
+++ src/main/java/io/github/mrnyax/StringPadderFactory.java
@@ -0,0 +1,15 @@
+package io.github.mrnyax;
+
+public final class StringPadderFactory
+{
+    private StringPadderFactory() {
+    }
+    /**
+     * Creates an instance of {@link StringPadder}.
+     *
+     * @return the new instance
+     */
+    public static StringPadder createStringPadder() {
+        return new StringPadderImpl();
+    }
+}
